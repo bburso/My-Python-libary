a5a11191b643ec7169aa5aa8c91f8b63a53ba6a2
Bryn Burson <bburso@users.noreply.github.com>
2018-11-01 13:30:45 +0100

    rename

--- Genpython1.py
+++
@@ -1,9 +0,0 @@
-#
-#   should show in Giton web now
-#
-nm1 = 'Bryn'
-tft = 35
-age = 54
-list1 = [1,2,3]
-string1 = 'I am ' + nm1 + ' I am '+ str(age) + ' years old and my list is:' + str(list1)
-print(string1)
